b6f508ff11c6cb0b54c8ce357a033ee9245edbfa
Manish Sarkar <sarkarmrigank28@gmail.com>
2024-10-30 02:16:12 +0530

    completed making network scanner

--- project/main.py
+++ project/main.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+import scapy.all as scapy
+
+def scan(ip_range):
+    arp_request = scapy.ARP(pdst = ip_range)
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0]
+
+    devices = []
+    for element in answered_list:
+        device_info = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        devices.append(device_info)
+
+    return devices
+
+ip_range = input("Enter the ip you want to scan-> ")
+devices = scan(ip_range)
+print("Devices found:")
+for device in devices:
+    print(f"IP: {device['ip']}, MAC: {device['mac']}")
